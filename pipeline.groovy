bd963e08982bb2d8c4470cf909ad812777df76da
Eric Sauer <esauer@redhat.com>
2017-01-23 19:48:46 -0500

    skopeo image

--- pipeline.groovy
+++ pipeline.groovy
@@ -13,7 +13,7 @@ node('master') {
   env.TOKEN = readFile('/var/run/secrets/kubernetes.io/serviceaccount/token').trim()
   env.OC_CMD = "oc --token=${env.TOKEN} --server=${ocpApiServer} --certificate-authority=/run/secrets/kubernetes.io/serviceaccount/ca.crt --namespace=${env.NAMESPACE}"
   env.APP_NAME = "${env.JOB_NAME}".replaceAll(/-?pipeline-?/, '').replaceAll(/-?${env.NAMESPACE}-?/, '')
-  env.SKOPEO_SLAVE_IMAGE = sh"${env.OC_CMD} get is ${env.APP_NAME} --template='{{ .status.dockerImageRepository }}'"
+  env.SKOPEO_SLAVE_IMAGE = sh "${env.OC_CMD} get is ${env.APP_NAME} --template=\'{{ .status.dockerImageRepository }}\'"
 
 }
 
